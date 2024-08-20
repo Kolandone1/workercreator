93851599b70fbb66805c4c68145ea9032e310856
Kolandone1 <kolandone1@gmail.com>
2024-08-20 21:39:09 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 
-# Update package list and install python
 pkg update && pkg upgrade
 pkg install -y python
 
-# Install requests module if not already installed
+
 pip install requests
-pkg install git
-# Clone your repository (replace with your actual repository URL)
-git clone https://github.com/Kolandone1/workercreator.git
 
-# Navigate to the script directory
-cd workercreator
+curl -o $PREFIX/bin/koland https://raw.githubusercontent.com/Kolandone1/workercreator/main/kol.py
 
-# Make the Python script executable
-chmod +x kol.py
-
-# Create an alias to run the script with the 'koland' command
-echo "alias koland='python $(pwd)/kol.py'" >> ~/.bashrc
-
-# Apply the changes to the current session
-source ~/.bashrc
+# Make the script executable
+chmod +x $PREFIX/bin/koland
 
 echo "Installation complete. You can now run the script using the 'koland' command."
