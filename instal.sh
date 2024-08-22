b7135d73de42a264c7b61bf8f045b4a33e61718a
Kolandone1 <kolandone1@gmail.com>
2024-08-22 23:54:10 +0330

    Update instal.sh

--- instal.sh
+++ instal.sh
@@ -5,6 +5,7 @@ proot-distro install ubuntu
 proot-distro login ubuntu
 apt update
 apt install nodejs
+apt-get install jq
 npm install -g wrangler
 exit
 bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/Selector/main/Sel.sh)
