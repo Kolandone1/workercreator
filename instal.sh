cd3731958eef1afcccda52368ecd9e6b7073687b
Kolandone1 <kolandone1@gmail.com>
2024-08-22 23:52:52 +0330

    Update instal.sh

--- instal.sh
+++ instal.sh
@@ -1,1 +1,10 @@
 
+pkg update && pkg upgrade
+pkg install proot-distro
+proot-distro install ubuntu
+proot-distro login ubuntu
+apt update
+apt install nodejs
+npm install -g wrangler
+exit
+bash <(curl -fsSL https://raw.githubusercontent.com/Kolandone/Selector/main/Sel.sh)
